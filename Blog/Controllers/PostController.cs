3f1f4f01ee488c6168bd109d2aa8345ccd2ea1a9
Wodson Luiz <wodsonluiz@live.com>
2019-02-04 22:29:17 -0200

    Aula 1

---
+++ Blog/Controllers/PostController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Blog.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers
+{
+    public class PostController : Controller
+    {
+        List<Post> listaPost = new List<Post>();
+
+        public PostController()
+        {
+            Post p = new Post();
+
+            p.Titulo = "Game of Thrones";
+            p.Resumo = "Baixo";
+            p.Categoria = "Série";
+            listaPost.Add(p);
+
+            ViewBag.Posts = listaPost;
+        }
+
+        public IActionResult Index()
+        {
+            return View(listaPost);
+        }
+
+        public IActionResult Novo()
+        {
+            return View();
+        }
+
+        public IActionResult AdicionaPost(Post novo)
+        {
+            listaPost.Add(novo);
+            return View("Index", listaPost);
+        }
+    }
+}
